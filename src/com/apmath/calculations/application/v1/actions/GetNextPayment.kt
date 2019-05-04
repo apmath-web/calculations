3cc7e5abaabf12365fe3c0883f5363231de6f5a2
malinink <apmath@malinink.ru>
2019-05-04 23:28:45 +0300

    Fix incorrect service interface deaclaration

--- src/com/apmath/calculations/application/v1/actions/GetNextPayment.kt
+++ src/com/apmath/calculations/application/v1/actions/GetNextPayment.kt
@@ -1,12 +1,13 @@
 package com.apmath.calculations.application.v1.actions
 
-import com.apmath.calculations.application.v1.models.*
-import com.apmath.calculations.domain.payments.list.PaymentsServiceInterface
+import com.apmath.calculations.application.v1.models.Mixed
+import com.apmath.calculations.application.v1.models.Payment
+import com.apmath.calculations.domain.payments.payment.PaymentServiceInterface
 import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
 import io.ktor.response.respond
 
-suspend fun ApplicationCall.v1GetNextPayment(paymentService: PaymentsServiceInterface) {
+suspend fun ApplicationCall.v1GetNextPayment(paymentService: PaymentServiceInterface) {
     val mixed = receive<Mixed>()
     val userId = getUserId(request)
     // validate, map, process
