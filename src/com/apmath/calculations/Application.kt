fac6908ea61e1ef963253d34b46164e68e7981bf
malinink <apmath@malinink.ru>
2019-05-04 23:18:52 +0300

    Add koin dependencies

--- src/com/apmath/calculations/Application.kt
+++ src/com/apmath/calculations/Application.kt
@@ -1,6 +1,7 @@
 package com.apmath.calculations
 
 import com.apmath.calculations.application.v1.v1
+import com.apmath.calculations.infrastructure.template
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
@@ -8,9 +9,10 @@ import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
 import io.ktor.locations.Locations
 import io.ktor.routing.Routing
+import org.koin.Logger.slf4jLogger
+import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
@@ -20,6 +22,11 @@ fun Application.module(testing: Boolean = false) {
     }
 
     install(DefaultHeaders) {
+    }
+
+    install(Koin) {
+        slf4jLogger()
+        modules(template)
     }
 
     install(ContentNegotiation) {
