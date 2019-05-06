99ba707b8a836c76a91c39af035c2528b22583ef
malinink <apmath@malinink.ru>
2019-05-06 22:55:21 +0300

    Add get all payments action logic

--- src/com/apmath/calculations/application/v1/actions/GetAllPayments.kt
+++ src/com/apmath/calculations/application/v1/actions/GetAllPayments.kt
@@ -1,30 +1,35 @@
 package com.apmath.calculations.application.v1.actions
 
-import com.apmath.calculations.application.v1.models.*
+import com.apmath.calculations.application.v1.actions.models.*
+import com.apmath.calculations.application.v1.mappers.incoming.OriginalLoanMapper
+import com.apmath.calculations.application.v1.mappers.outgoing.PaymentsMapper
 import com.apmath.calculations.application.v1.validators.LoanBuilder
 import com.apmath.calculations.domain.payments.list.PaymentsServiceInterface
 import com.apmath.validation.simple.*
 import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
 import io.ktor.response.respond
-import org.modelmapper.ModelMapper
 
 suspend fun ApplicationCall.v1GetAllPayments(paymentsService: PaymentsServiceInterface) {
     val loan = receive<Loan>()
 
     val validator = LoanBuilder()
         .prepend("amount", RequiredValidator())
+        .prepend("term", RequiredValidator())
+        .prepend("interest", RequiredValidator())
         .prepend("date", RequiredValidator())
-        .prepend("term", NullableValidator(true))
         .prepend("rounding", NullableValidator(true))
+        .prepend("regularPaymentAmount", NullableValidator(true))
+        .prepend("remainingTerm", NullableValidator(true))
         .build()
 
     if (!validator.validate(loan)) {
         respond(validator.messages)
     }
 
-    val mapper = ModelMapper()
+    val originalLoan = OriginalLoanMapper().map(loan)
 
-    // validate, map, process
-    respond(arrayListOf(Payment(), Payment()))
+    val payments = paymentsService.generatePayments(originalLoan)
+
+    respond(PaymentsMapper().map(payments))
 }
