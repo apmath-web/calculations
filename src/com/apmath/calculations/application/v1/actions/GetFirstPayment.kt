4d06d14e4598fc7a0f4f971b8deac823d6697975
malinink <apmath@malinink.ru>
2019-05-06 22:56:01 +0300

    Imports fixes

--- src/com/apmath/calculations/application/v1/actions/GetFirstPayment.kt
+++ src/com/apmath/calculations/application/v1/actions/GetFirstPayment.kt
@@ -1,7 +1,7 @@
 package com.apmath.calculations.application.v1.actions
 
-import com.apmath.calculations.application.v1.models.Mixed
-import com.apmath.calculations.application.v1.models.Payment
+import com.apmath.calculations.application.v1.actions.models.Mixed
+import com.apmath.calculations.application.v1.actions.models.Payment
 import com.apmath.calculations.domain.payments.payment.PaymentServiceInterface
 import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
--- src/com/apmath/calculations/application/v1/actions/ProcessNewLoan.kt
+++ src/com/apmath/calculations/application/v1/actions/ProcessNewLoan.kt
@@ -1,6 +1,6 @@
 package com.apmath.calculations.application.v1.actions
 
-import com.apmath.calculations.application.v1.models.*
+import com.apmath.calculations.application.v1.actions.models.*
 import com.apmath.calculations.domain.init.LoanInitServiceInterface
 import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
