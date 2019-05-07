c9cbe1c126a9a1466ae22ee961e574738f43fa7e
malinink <apmath@malinink.ru>
2019-05-07 22:53:09 +0300

    Fix Mixed model properties order

--- src/com/apmath/calculations/application/v1/actions/models/Mixed.kt
+++ src/com/apmath/calculations/application/v1/actions/models/Mixed.kt
@@ -3,6 +3,6 @@ package com.apmath.calculations.application.v1.actions.models
 
 class Mixed {
     var loan: Loan? = null
-    var payment: Payment? = null
     var lastPayment: Payment? = null
+    var payment: Payment? = null
 }
