35b31e23aae3a6ab1d893e4fd90820dcd40d31e4
malinink <apmath@malinink.ru>
2019-05-16 22:42:19 +0300

    Add exception application level interceptors, trow correct BadRequest validation exceptions

--- src/com/apmath/calculations/application/v1/actions/GetNextPayment.kt
+++ src/com/apmath/calculations/application/v1/actions/GetNextPayment.kt
@@ -1,12 +1,15 @@
 package com.apmath.calculations.application.v1.actions
 
 import com.apmath.calculations.application.v1.actions.models.Mixed
+import com.apmath.calculations.application.v1.exceptions.BadRequestException
+import com.apmath.calculations.application.v1.exceptions.BadRequestValidationException
 import com.apmath.calculations.application.v1.mappers.incoming.PresentLoanWithPaymentMapper
 import com.apmath.calculations.application.v1.mappers.outgoing.PaymentWithInternalDataMapper
 import com.apmath.calculations.application.v1.validators.LoanBuilder
 import com.apmath.calculations.application.v1.validators.MixedBuilder
 import com.apmath.calculations.application.v1.validators.PaymentBuilder
 import com.apmath.calculations.domain.payments.payment.PaymentServiceInterface
+import com.apmath.calculations.domain.payments.payment.exceptions.*
 import com.apmath.validation.simple.NullableValidator
 import com.apmath.validation.simple.RequiredValidator
 import io.ktor.application.ApplicationCall
@@ -48,12 +51,25 @@ suspend fun ApplicationCall.v1GetNextPayment(paymentService: PaymentServiceInter
         .build()
 
     if (!validator.validate(mixed)) {
-        respond(validator.messages)
+        throw BadRequestValidationException(validator)
     }
 
     val presentLoanWithPayment = PresentLoanWithPaymentMapper().map(mixed.loan!!, mixed.lastPayment!!, mixed.payment!!)
 
-    val paymentWithInternalData = paymentService.writeOf(presentLoanWithPayment)
-
-    respond(PaymentWithInternalDataMapper().map(paymentWithInternalData))
+    try {
+        val paymentWithInternalData = paymentService.writeOf(presentLoanWithPayment)
+        respond(PaymentWithInternalDataMapper().map(paymentWithInternalData))
+    } catch (e: CreditAlreadyPaidException) {
+        throw BadRequestException("Credit already paid")
+    } catch (e: PaymentDateMoreThanNextPaymentDateException) {
+        throw BadRequestException("Payment date cannot be less than last payment date")
+    } catch (e: PaymentDateMoreThanNextPaymentDateException) {
+        throw BadRequestException("Payment date cannot be more than next payment date")
+    } catch (e: PaymentLessThanMinimalException) {
+        throw BadRequestException("Payment amount should be more than minimum of 100")
+    } catch (e: PaymentLessThanRegularException) {
+        throw BadRequestException("Payment amount can not be less than regular payment amount")
+    } catch (e: PaymentMoreThanFullEarlyRepaimentException) {
+        throw BadRequestException("Payment amount cannot be more than credit full early repaiment amount")
+    }
 }
